ff5b686252123eddf2d0f29d392fda822db37766
Matthew Twomey <mtwomey@beakstar.com>
2019-10-01 23:56:37 -0500

    Modify new config

--- config/default.js
+++ config/default.js
@@ -53,7 +53,7 @@ module.exports = {
             scope:                       'openid profile',
             clientId:                    'C7q8jpZfTtRRDc4n3os9lhbweec6Odlo',
             redirectUri:                 process.env.REDIRECT_URI,
-            clientSecret:                process.env[CLIENT_SECRET_SAMLTESTIDP]
+            clientSecret:                process.env.CLIENT_SECRET_SAMLTESTIDP
         }
     },
 
